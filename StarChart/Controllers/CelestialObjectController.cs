933e8b7ca135a14c531665887e557ecbf4a5aee5
Rotciv <leonelvic86@gmail.com>
2021-09-01 21:33:20 -0700

    CelestialObjectController_GetActions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,6 +17,42 @@ namespace StarChart.Controllers
             _context = ctx;
 
         }
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var Co = _context.CelestialObjects.Find(id);
+            if (Co == null)
+                return NotFound();
+           
+           
+            if(Co.Id == Co.OrbitedObjectId)
+                Co.Satellites.Add(Co);
+            return Ok(Co);
+            
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var Co = _context.CelestialObjects.FirstOrDefault(op => op.Name == name);
+            if (Co == null)
+                return NotFound();
+            if (Co.Id == Co.OrbitedObjectId)
+                Co.Satellites.Add(Co);
+            return Ok(Co);
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+          var   Co = _context.CelestialObjects.ToList();
+           
+        for (var i =0; i< Co.Count; i++)
+            {
+                if (Co[i].Id == Co[i].OrbitedObjectId)
+                    Co[i].Satellites.Add(Co[i]);
+            }
+            return Ok(Co);
+
+        }
         
     }
 }
